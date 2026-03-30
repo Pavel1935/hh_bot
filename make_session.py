e7ef230198276739b5ae6e8a38e59fc6f78fc98d
Pavel1935 <oukb1147@gmail.com>
2026-03-30 12:21:03 +0300

    save local changes

--- make_session.py
+++ make_session.py
@@ -2,8 +2,8 @@ import asyncio
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 
-API_ID = 37531198
-API_HASH = "d4b3a4ee64a1e2d2981e6ebb943e518d"
+API_ID = 0
+API_HASH = "PUT_YOUR_API_HASH_HERE"
 
 async def main():
     async with TelegramClient(StringSession(), API_ID, API_HASH) as client:
